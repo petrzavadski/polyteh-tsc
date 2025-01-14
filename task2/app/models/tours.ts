37c25ecadf20c47972d4d0653ec55a0f9106c5be
Petr <petrzavadski@gmail.com>
2025-01-14 13:59:35 +0300

    Second Task

---
+++ task2/app/models/tours.ts
@@ -0,0 +1,15 @@
+// description: "From the south to the center of the country"
+// id: "1"
+// img: "ocean.jpg"
+// name: "Mexico"
+// price: "€2,192"
+// tourOperator: "LocalAdventures"
+
+export interface ITours {
+    description: string;
+    id: string;
+    img: string;
+    name: string;
+    price: string;
+    tourOperator: string;
+}
---
+++ task2/app/models/user.ts
@@ -0,0 +1,6 @@
+export interface IUser {
+    name: string;
+    cardNumber: string;
+    birthDate: Date;
+}
+
